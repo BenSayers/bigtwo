01b1c920cc53875407840ef5a9e945a2b9c0756e
Ben Sayers <bsayers@atlassian.com>
2014-05-21 00:34:19 +1000

    fix(tests): timing issue in test runner where tests were loaded before jasmine-jquery

--- test/support/karma.config.js
+++ test/support/karma.config.js
@@ -5,9 +5,9 @@ module.exports = function (config) {
         captureTimeout: 120000,
         files: [
             'src/js/require.config.js',
-            'test/support/test-runner.js',
             'bower_components/jquery/dist/jquery.js',
             'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
+            'test/support/test-runner.js',
             {pattern: 'src/**/*.js', included: false},
             {pattern: 'test/**/*.js', included: false},
             {pattern: 'bower_components/**/*.js', included: false}
